11a2469cec5deb43ed7d84924f8a412f6c3c5448
Kateryna Boikova <kateboikova@gmail.com>
2020-10-02 16:25:24 +0200

    hm 8 updated

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,13 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileHerohide = document.querySelector("[data-menu-hero]");
 
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
-    mobileHerohide.classList.toggle("is-open");
   });
 })();
